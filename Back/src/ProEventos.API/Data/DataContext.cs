0fb0c775c5ee98103608b1f5e44d3caacf81d836
satella <89879456+satellasz@users.noreply.github.com>
2022-07-27 10:31:03 -0300

    Criação de DbContext e injeção ao EventoController

---
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using ProEventos.API.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ProEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ProEventos.API.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProEventos.API
 {
@@ -26,6 +22,11 @@ namespace ProEventos.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //######## Configuração de context #########
+            services.AddDbContext<DataContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
