7baf9043a5dce9e5a01167fd6b01fb4c13207140
satella <89879456+satellasz@users.noreply.github.com>
2022-07-28 12:22:33 -0300

    Angular: interpolação, diretiva e binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = []
+  public eventosFiltrados: any = []
+
+  widthImg: number = 125
+  marginImg: number = 2
+  isCollapsed: boolean = false
+  private _filtroLista: string = ''
+
+  public get filtroLista(): string {
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos
+  }
+
+  public filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase()
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos()
   }
 
+  alterarImagem(): void {
+    this.isCollapsed = !this.isCollapsed
+  }
+
   public getEventos(): void {
     this.http.get("https://localhost:5001/api/Evento").subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     )
   }
